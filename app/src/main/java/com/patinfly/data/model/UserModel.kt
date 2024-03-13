3a910316243bd0550d71633bc27cd1dd64784358
Oreste <or.greco@studenti.unina.it>
2024-03-13 09:50:20 +0100

    Aggiustamenti

---
+++ app/src/main/java/com/patinfly/data/model/UserModel.kt
@@ -0,0 +1,14 @@
+package com.patinfly.data.model
+
+import java.util.Date
+import java.util.UUID
+
+data class UserModel(
+    val uuid: UUID,
+    val username: String,
+    val email: String,
+    val isRenting: Boolean,
+    val scooterRented: UUID,
+    val creationDate: Date,
+    val numberOfRents: Int
+)
--- app/src/main/java/com/patinfly/domain/repository/IRentRepository.kt
+++ app/src/main/java/com/patinfly/domain/repository/IRentRepository.kt
@@ -3,7 +3,7 @@ package com.patinfly.domain.repository
 import com.patinfly.domain.model.Rent
 import java.util.UUID
 
-interface RentRepository {
+interface IRentRepository {
     fun fetchRents(): List<Rent>
     fun fetchRentByUUID(uuid: UUID): Rent?
     fun saveRent(rent: Rent)
